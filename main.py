c0b01a7adc8aadc41a22f8164f5e857e5b1aa52e
むずでょ <muzudho1@gmail.com>
2021-08-15 23:32:16 +0900

    表示

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from csa_file import CsaFile
 intervalSeconds = 15 # 棋譜読取間隔15秒
 heartBeatSeconds = 0 # 画面が止まってないことをアピールするために増えていくだけの数
 score = []
-# while True:
-if True:
+while True:
 
     try:
         # 棋譜ファイル（CSA形式）のURLを指定してください
@@ -28,16 +27,16 @@ if True:
         print(f'ハートビート {heartBeatSeconds}') # 生きてますよ
         heartBeatSeconds += intervalSeconds
         time.sleep(intervalSeconds)
-        # TODO continue
+        continue
 
     # 残り時間が変わらなかったら更新しない
     if score == [0, csaFile.enteringKingScoreing[1], csaFile.enteringKingScoreing[2]] :
         heartBeatSeconds += intervalSeconds
         time.sleep(intervalSeconds)
-        # TODO continue
+        continue
 
     # Windows用のコマンド　コンソール消去
-    # os.system('cls')
+    os.system('cls')
 
     # 残り時間の算出
     score = csaFile.enteringKingScoreing
@@ -55,13 +54,14 @@ if True:
     print(f'    大会モード {csaFile.tournament}') 
     print(f'    URL {csaFile.url}') 
     # 盤表示
-    csaFile.printBoard()
+    # csaFile.printBoard()
     print(f'    ハートビート {heartBeatSeconds}') # 生きてますよ
 
     if csaFile.endTime:
         # 終了してんだったら　ループを抜けよ（＾～＾）
+        print(f'    終了時刻 {csaFile.endTime}')
         print(f'おわり')
-        # TODO break
+        break
 
     heartBeatSeconds += intervalSeconds
     time.sleep(intervalSeconds)
